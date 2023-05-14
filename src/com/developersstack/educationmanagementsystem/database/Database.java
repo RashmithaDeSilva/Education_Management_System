ed934e1c358da08dfd91a17de21b6c53a63742e8
Rashmitha De Silva <lahirurashmitha00@gmail.com>
2023-05-15 04:45:37 +0530

    Complete user model

--- src/com/developersstack/educationmanagementsystem/database/Database.java
+++ src/com/developersstack/educationmanagementsystem/database/Database.java
@@ -1,4 +1,17 @@
 package com.developersstack.educationmanagementsystem.database;
 
+import com.developersstack.educationmanagementsystem.model.User;
+
+import java.util.ArrayList;
+
 public class Database {
+
+    private ArrayList<User> usersArrayList = new ArrayList<User>();
+
+    public ArrayList<User> getUsersArrayList() {
+        return usersArrayList;
+    }
+    public void setUsersArrayList(ArrayList<User> usersArrayList) {
+        this.usersArrayList = usersArrayList;
+    }
 }
