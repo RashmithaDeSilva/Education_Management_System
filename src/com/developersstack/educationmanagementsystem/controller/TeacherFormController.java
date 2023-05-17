72ffcc978f38ea36077decebd8d2f40515286046
Rashmitha De Silva <lahirurashmitha00@gmail.com>
2023-05-17 14:23:45 +0530

    TeacherForm.xml complete UI desing an add all components in to Controller class

---
+++ src/com/developersstack/educationmanagementsystem/controller/TeacherFormController.java
@@ -0,0 +1,33 @@
+package com.developersstack.educationmanagementsystem.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class TeacherFormController {
+    public AnchorPane contaxtTeacherManagement;
+    public Button btnSaveAndUpdateTeacher;
+    public TextField txtCode;
+    public TextField txtName;
+    public TextField txtContactNubmer;
+    public TextField txtAddress;
+    public TextField txtSearch;
+    public TableView tblTeacher;
+    public TableColumn colCode;
+    public TableColumn colName;
+    public TableColumn colContactNumber;
+    public TableColumn colAddress;
+    public TableColumn colOption;
+
+    public void addNewTeacherOnAction(ActionEvent actionEvent) {
+    }
+
+    public void saveAndUpdateTeacherOnAction(ActionEvent actionEvent) {
+    }
+
+    public void homeOnAction(ActionEvent actionEvent) {
+    }
+}
