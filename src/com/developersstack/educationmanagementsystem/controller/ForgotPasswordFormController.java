d4670225ddbf5a0f0ad808eb1b8bb64990ef15c1
Rashmitha De Silva <lahirurashmitha00@gmail.com>
2023-05-18 00:03:00 +0530

    Create and Dising UI Forgot Password Form and Controller

---
+++ src/com/developersstack/educationmanagementsystem/controller/ForgotPasswordFormController.java
@@ -0,0 +1,16 @@
+package com.developersstack.educationmanagementsystem.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class ForgotPasswordFormController {
+    public AnchorPane contectForgotPassword;
+    public TextField txtEmail;
+
+    public void sendVerificationOnAction(ActionEvent actionEvent) {
+    }
+
+    public void backtoLoginPageOnAction(ActionEvent actionEvent) {
+    }
+}
